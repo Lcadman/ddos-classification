dd81916ef0c9867aed1145a466fa08a160b9ce4d
Chad Schwenke <chad.schwenke@colostate.edu>
2024-04-16 17:20:14 -0600

    updated slurm params

--- run_train.sh
+++ run_train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name="DDoS-Classification"
 #SBATCH --partition=kestrel-gpu
-#SBATCH --qos=gpu_short
+#SBATCH --qos=gpu_debug
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=500M
-#SBATCH --gres=gpu:3090:2
+#SBATCH --mem-per-cpu=1G
+#SBATCH --gres=gpu:nvidia_a100_3g.39gb:2
 #SBATCH --time=00:05:00
 
 source /s/bach/b/class/cs535/cs535b/ddos-classification/venv/bin/activate
